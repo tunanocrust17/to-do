91389a386aee8790b61e82ee0425787278f73904
Eric Huet <ewhuet@gmail.com>
2024-02-23 21:02:20 +0200

    set up the base file structure with a /src folder including an index.html and index.js, installed webpack and the needed dependencies

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To Do',
+      filename: "index.html",
+      template: path.resolve(__dirname, "src", "index.html"),
+      inject: true,
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
