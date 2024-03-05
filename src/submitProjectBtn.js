f3d4b5d626fce63df1575de4ac653a0533f0fd56
ewhuet <ewhuet@gmail.com>
2024-03-05 12:11:22 +0200

    update the module so it will clear the form and close the dialog box when the user submits

--- src/submitProjectBtn.js
+++ src/submitProjectBtn.js
@@ -2,9 +2,13 @@ import { addNewProject } from "./CRUD/addNewProject";
 
 export function submitProjectBtn() {
     const addProjectBtn = document.querySelector('.add-project-btn')
+    const projectDialog = document.querySelector('.project')
+    const projectForm = document.querySelector('.newProject')
 
     addProjectBtn.addEventListener('click', (e)=>{
         e.preventDefault();
         addNewProject();
+        projectForm.reset();
+        projectDialog.close();
     })
 }
