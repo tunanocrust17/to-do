533f01e3821e69b346f5332353b364a970f513b4
ewhuet <ewhuet@gmail.com>
2024-03-06 13:15:04 +0200

    modified to include removeProjectContainer, chooseProject and addProjectUI

--- src/submitProjectBtn.js
+++ src/submitProjectBtn.js
@@ -1,14 +1,20 @@
-import { addNewProject } from "./CRUD/addNewProject";
+import addNewProject from "./CRUD/addNewProject";
+import chooseProject from "./UI/readProjectUI";
+import removeProjectContainer from "./UI/removeProjectContainer";
+import addProjectUI from "./UI/addProjectUI";
 
-export function submitProjectBtn() {
+export default function submitProjectBtn() {
     const addProjectBtn = document.querySelector('.add-project-btn')
     const projectDialog = document.querySelector('.project')
     const projectForm = document.querySelector('.newProject')
 
     addProjectBtn.addEventListener('click', (e)=>{
         e.preventDefault();
+        removeProjectContainer();
         addNewProject();
+        addProjectUI();
         projectForm.reset();
         projectDialog.close();
+        chooseProject(); //adds functionality for user to click on the project in the sidebar and it will run a function
     })
 }
