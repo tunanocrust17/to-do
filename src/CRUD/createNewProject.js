f37bc908b7ed10d0ce65c58b6fc90065ff483f18
ewhuet <ewhuet@gmail.com>
2024-03-06 15:29:59 +0200

    revamped to include the addProjectUI and turned into a factory function to enable it to hold an instance of the the currentLibrary

--- src/CRUD/createNewProject.js
+++ src/CRUD/createNewProject.js
@@ -1,8 +1,58 @@
-export default function addProjectButton(){
-    const addProject = document.querySelector(".new-list-div")
-    const newProjectDialog = document.querySelector(".project")
+import projectLibrary from "../dataModels/projectLibrary";
+import Project from "../dataModels/project";
 
-    addProject.addEventListener('click', ()=>{
-        newProjectDialog.showModal();
-    })
+let currentLibrary = projectLibrary();
+
+export default function addNewProject() {
+
+
+    const createProject = ()=> {
+    let projectTitle = document.getElementById('project-title').value
+
+    let newProjectID = Math.floor(Date.now()*Math.random()).toString();
+
+    let newProject = new Project (
+        newProjectID,
+        projectTitle
+    )
+
+    currentLibrary.push(newProject);
+    console.log(currentLibrary)
+    return currentLibrary;
+}
+
+    const addProjectUI = ()=> {
+
+        
+        currentLibrary.forEach((item)=>{
+
+            const element = document.querySelector('.projects-list');
+
+            const projectContainer= document.createElement('div');
+            const projectBtn = document.createElement('button');
+            const btnSpan = document.createElement('span');
+            const projectTitleH4 = document.createElement('h4');
+        
+            
+            btnSpan.classList.add('material-symbols-outlined');
+            btnSpan.innerHTML='lists';
+            projectBtn.appendChild(btnSpan);
+            
+            projectBtn.classList.add('icon-buttons');
+            projectContainer.appendChild(projectBtn)
+        
+            projectTitleH4.innerHTML= item.title;
+            projectContainer.appendChild(projectTitleH4);
+        
+            projectContainer.classList.add('project-container');
+            projectContainer.setAttribute('listid', item.id);
+            element.appendChild(projectContainer);
+        })
+    }
+
+    return {
+        currentLibrary,
+        createProject,
+        addProjectUI
+    }
 }
