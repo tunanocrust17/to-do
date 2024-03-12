122693e388e074ca6c369c93cf250a0c7fcba7e8
Eric Huet <ewhuet@gmail.com>
2024-03-12 12:40:47 +0200

    cleaned up the module to only create the new project object and save it into local storage

--- src/CRUD/createNewProject.js
+++ src/CRUD/createNewProject.js
@@ -1,13 +1,10 @@
-import projectLibrary from "../dataModels/projectLibrary";
 import Project from "../dataModels/project";
 import getDataFromLocalStorage from "../Storage/getDataFromLocalStorage";
 import setDataToLocalStorage from "../Storage/setDataToLocalStorage";
 
 
-export default function addNewProject() {
+export default function createNewProject() {
 
-
-    const createProject = ()=> {
 
     if(getDataFromLocalStorage('todo') == null ) {
         localStorage.setItem('todo', '[]');
@@ -28,42 +25,4 @@ export default function addNewProject() {
 
     setDataToLocalStorage('todo', old_data)
 
-}
-
-    const addProjectUI = ()=> {
-
-        var localData = getDataFromLocalStorage('todo');
-        console.log(localData)
-        
-        localData.forEach((item)=>{
-
-            const element = document.querySelector('.projects-list');
-
-            const projectContainer= document.createElement('div');
-            const projectBtn = document.createElement('button');
-            const btnSpan = document.createElement('span');
-            const projectTitleH4 = document.createElement('h4');
-        
-            
-            btnSpan.classList.add('material-symbols-outlined');
-            btnSpan.innerHTML='lists';
-            projectBtn.appendChild(btnSpan);
-            
-            projectBtn.classList.add('icon-buttons');
-            projectContainer.appendChild(projectBtn)
-        
-            projectTitleH4.innerHTML= item.title;
-            projectContainer.appendChild(projectTitleH4);
-        
-            projectContainer.classList.add('project-container');
-            projectContainer.setAttribute('listid', item.id);
-            element.appendChild(projectContainer);
-        })
-    }
-
-    return {
-
-        createProject,
-        addProjectUI
-    }
 }
