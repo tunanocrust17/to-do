5f9dad7022c3ce732c4b219d839886c24f264c39
Eric Huet <ewhuet@gmail.com>
2024-03-12 12:41:36 +0200

    updated to get data from local storage

--- src/CRUD/readProject.js
+++ src/CRUD/readProject.js
@@ -1,17 +1,19 @@
-import addNewProject from "./createNewProject";
+import getDataFromLocalStorage from "../Storage/getDataFromLocalStorage";
 
 export default function readProject() {
     let projectContainer = document.querySelectorAll('.project-container');
     let projectTitle = document.querySelector('.project-title')
 
+    let parsedData = getDataFromLocalStorage('todo');
+
     projectContainer.forEach(item => {
         item.addEventListener('click',(e)=>{
             let projectChosen = e.target.getAttribute('listid');
-            let index = addNewProject().currentLibrary.map((item)=>item.id).indexOf(projectChosen);
+            let index = parsedData.map((item)=>item.id).indexOf(projectChosen);
             console.log(index);
             console.log(projectChosen);
-            console.log(addNewProject().currentLibrary[index])
-            let clickedProject = addNewProject().currentLibrary[index];
+            console.log(parsedData[index])
+            let clickedProject = parsedData[index];
             projectTitle.innerHTML = clickedProject.title;
             return projectChosen;
         })
