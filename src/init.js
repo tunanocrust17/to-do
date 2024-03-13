8413576e479d5fb36ab74d7f2835ddbb2204aa02
Eric Huet <ewhuet@gmail.com>
2024-03-13 15:59:22 +0200

    added the addToDoButton

--- src/init.js
+++ src/init.js
@@ -2,11 +2,13 @@ import './styles/styles.css'
 import createNewProjectUI from './UI/createNewProjectUI';
 import addProjectButton from './UI/newProjectBtn';
 import submitProjectBtn from './UI/submitProjectBtn';
+import addToDoButton from './UI/newToDoBtn';
 import readProject from './CRUD/readProject';
 
 export default function init(){
     createNewProjectUI();
-    addProjectButton()
+    addProjectButton();
+    addToDoButton();
     submitProjectBtn();
     readProject();
 }
