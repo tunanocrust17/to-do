4333437b734a957204652778b87caf3ce2927f79
Eric Huet <ewhuet@gmail.com>
2024-03-13 15:56:56 +0200

    added the removeProjectsFromList to run so the list will only contain the current items and not duplicate by adding them to an existing list

--- src/UI/newToDoBtn.js
+++ src/UI/newToDoBtn.js
@@ -1,10 +1,12 @@
 import retrieveProjects from "./retrieveProjects";
+import removeProjectsFromList from "./removeProjectChildren";
 
 export default function addToDoButton() {
     const addToDo = document.querySelector('.new-todo-div');
     const newToDoDialog = document.querySelector('.todo')
 
     addToDo.addEventListener('click', ()=>{
+        removeProjectsFromList();
         retrieveProjects();
         newToDoDialog.showModal();
     })
