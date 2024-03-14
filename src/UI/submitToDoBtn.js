88cf590fa73e33f0d5be4c1ca23e8166a188a486
Eric Huet <ewhuet@gmail.com>
2024-03-14 21:36:40 +0200

    reset the form and close the dialog when the user clicks add task

--- src/UI/submitToDoBtn.js
+++ src/UI/submitToDoBtn.js
@@ -2,9 +2,13 @@ import createNewToDo from "../CRUD/createNewToDo";
 
 export default function submitToDoBtn(){
     const addToDoBtn = document.querySelector('.add-todo-btn');
+    const todoDialog = document.querySelector('.todo');
+    const todoForm = document.querySelector('.newToDo')
 
     addToDoBtn.addEventListener('click', (e)=>{
         e.preventDefault();
         createNewToDo();
+        todoForm.reset();
+        todoDialog.close();
     })
 }
