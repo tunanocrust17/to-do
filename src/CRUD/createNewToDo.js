5db6988c564d8cc552dd7c96748a9929063bb873
Eric Huet <ewhuet@gmail.com>
2024-03-15 21:24:46 +0200

    updated to included the new project id parameter

--- src/CRUD/createNewToDo.js
+++ src/CRUD/createNewToDo.js
@@ -18,6 +18,7 @@ export default function createNewToDo(){
 
     let newToDo = new Todo (
         newToDoID,
+        selectedProject,
         todoTitle,
         todoDescription,
         dueDate,
