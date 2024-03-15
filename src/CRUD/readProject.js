170ede18ee5f259acb15fe763d3673518f957e0b
Eric Huet <ewhuet@gmail.com>
2024-03-15 21:23:00 +0200

    added functionality to remove any existing to do items and then render the items of the new item clicked

--- src/CRUD/readProject.js
+++ src/CRUD/readProject.js
@@ -7,7 +7,7 @@ export default function readProject() {
     let parsedData = getDataFromLocalStorage('todo');
 
     projectContainer.forEach(item => {
-        item.addEventListener('click',(e)=>{
+        item.addEventListener('click', (e)=>{
             let projectChosen = e.target.getAttribute('listid');
             let index = parsedData.map((item)=>item.id).indexOf(projectChosen);
             console.log(index);
@@ -15,7 +15,44 @@ export default function readProject() {
             console.log(parsedData[index])
             let clickedProject = parsedData[index];
             projectTitle.innerHTML = clickedProject.title;
-            return projectChosen;
+
+
+            
+            //removes an to do items that are currently shown
+            let element = document.querySelector('.content-container');
+            let childDiv = document.querySelectorAll('.todo-box');
+            
+            if (childDiv != null){
+            childDiv.forEach((item)=>{
+                item.remove();
+                })
+            }
+            /////////////
+
+            //building the list of to do's to display to the user
+            let itemsArray = parsedData[index].items;
+            console.log(itemsArray);
+
+            itemsArray.forEach((item)=>{
+                
+                let todoBox = document.createElement('div');
+                let todoTitle = document.createElement('h3');
+                let dueDate = document.createElement('p');
+                
+    
+                todoTitle.innerHTML = item.title;
+                todoBox.appendChild(todoTitle);
+
+                dueDate.innerHTML = item.dueDate
+                todoBox.appendChild(dueDate);
+
+                todoBox.classList.add('todo-box');
+                element.appendChild(todoBox);
+            })
+            
+            
+
+
         })
     });
 }
