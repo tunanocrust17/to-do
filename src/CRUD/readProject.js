1b96704298af98faafe0b9feec5cbb68ef74ca87
Eric Huet <ewhuet@gmail.com>
2024-03-20 14:35:33 +0200

    added in a delete button icon for the user to click and call the deleteProject function which adds an event listener to the delete button for the user to remove the project

--- src/CRUD/readProject.js
+++ src/CRUD/readProject.js
@@ -1,5 +1,6 @@
 import getDataFromLocalStorage from "../Storage/getDataFromLocalStorage";
 import toDoFormat from "../UI/toDoFormat";
+import deleteProject from "./deleteProject";
 
 export default function readProject() {
     let projectContainer = document.querySelectorAll('.project-container');
@@ -20,6 +21,18 @@ export default function readProject() {
             let clickedProject = parsedData[index];
             projectTitle.innerHTML = clickedProject.title;
             projectTitle.setAttribute('listid', projectChosen);
+
+            //create the delete button for the user to delete the project
+            let deleteButton = document.createElement('span');
+            
+
+            deleteButton.classList.add('delete-button');
+            deleteButton.classList.add('material-symbols-outlined')
+            deleteButton.innerHTML = "delete"
+            deleteButton.setAttribute('listid', projectChosen)
+            projectTitle.appendChild(deleteButton);
+
+            deleteProject();
 
             toDoFormat(parsedData, index);
 
