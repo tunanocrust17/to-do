bf335219c6b1d7e896070cc4bc49d1b49e196ade
Eric Huet <ewhuet@gmail.com>
2024-03-20 11:17:15 +0200

    replaced the code to render the to do items and replaced it with the toDoFormat

--- src/CRUD/readProject.js
+++ src/CRUD/readProject.js
@@ -1,57 +1,27 @@
 import getDataFromLocalStorage from "../Storage/getDataFromLocalStorage";
+import toDoFormat from "../UI/toDoFormat";
 
 export default function readProject() {
     let projectContainer = document.querySelectorAll('.project-container');
     let projectTitle = document.querySelector('.project-title')
 
-    let parsedData = getDataFromLocalStorage('todo');
+   
 
     projectContainer.forEach(item => {
         item.addEventListener('click', (e)=>{
+
+            let parsedData = getDataFromLocalStorage('todo');
+
             let projectChosen = e.target.getAttribute('listid');
             let index = parsedData.map((item)=>item.id).indexOf(projectChosen);
             console.log(index);
             console.log(projectChosen);
-            console.log(parsedData[index])
+            console.log(parsedData[index]);
             let clickedProject = parsedData[index];
             projectTitle.innerHTML = clickedProject.title;
+            projectTitle.setAttribute('listid', projectChosen);
 
-
-            
-            //removes an to do items that are currently shown
-            let element = document.querySelector('.content-container');
-            let childDiv = document.querySelectorAll('.todo-box');
-            
-            if (childDiv != null){
-            childDiv.forEach((item)=>{
-                item.remove();
-                })
-            }
-            /////////////
-
-            //building the list of to do's to display to the user
-            let itemsArray = parsedData[index].items;
-            console.log(itemsArray);
-
-            itemsArray.forEach((item)=>{
-                
-                let todoBox = document.createElement('div');
-                let todoTitle = document.createElement('h3');
-                let dueDate = document.createElement('p');
-                
-    
-                todoTitle.innerHTML = item.title;
-                todoBox.appendChild(todoTitle);
-
-                dueDate.innerHTML = item.dueDate
-                todoBox.appendChild(dueDate);
-
-                todoBox.classList.add('todo-box');
-                element.appendChild(todoBox);
-            })
-            
-            
-
+            toDoFormat(parsedData, index);
 
         })
     });
