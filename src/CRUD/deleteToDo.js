52e87c3076a39b80812975939d31deb2c8041c43
Eric Huet <ewhuet@gmail.com>
2024-03-20 15:40:15 +0200

    created the functionality to find the item the user is deleting and remove from the array

---
+++ src/CRUD/deleteToDo.js
@@ -0,0 +1,35 @@
+import getDataFromLocalStorage from "../Storage/getDataFromLocalStorage";
+import setDataToLocalStorage from "../Storage/setDataToLocalStorage";
+import toDoFormat from "../UI/toDoFormat";
+
+
+export default function deleteToDo(){
+    let deleteToDoBtn = document.querySelectorAll('.delete-todo');
+
+    deleteToDoBtn.forEach((item)=>{
+        item.addEventListener('click', (e)=>{
+
+            let oldData = getDataFromLocalStorage('todo');
+
+            //retrieves the current project ID and task ID from the clicked element
+            let currentProjectID = e.target.getAttribute('projectid');
+            let clickedToDoID = e.target.getAttribute('todoid');
+
+            //finds the index of the project clicked within the local storage array
+            let currentProjectIndex = oldData.map((item)=>item.id).indexOf(currentProjectID);
+
+            //creates a variable that's the array of items for the current project being displayed
+            let clickedProject = oldData[currentProjectIndex].items;
+        
+            //finds the index of the element the user is deleting within the clicked project array of all the items
+            let clickedToDoIndex = clickedProject.map((item)=>item.id).indexOf(clickedToDoID);
+
+            //removes the task from the items array and then saves the updated data to local storage
+            clickedProject.splice(clickedToDoIndex,1);
+            setDataToLocalStorage('todo', oldData);
+
+            toDoFormat(oldData, currentProjectIndex)
+
+        })
+    })
+}
