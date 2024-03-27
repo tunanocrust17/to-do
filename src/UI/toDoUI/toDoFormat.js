969ca0e25f0c58c7797520dc5290a4998cfe55ae
Eric Huet <ewhuet@gmail.com>
2024-03-27 10:53:17 +0200

    added a new info button to expand the todo and display all the fields for the user to read and edit

--- src/UI/toDoUI/toDoFormat.js
+++ src/UI/toDoUI/toDoFormat.js
@@ -1,7 +1,8 @@
 import deleteToDo from "../../CRUD/deleteToDo";
+import readToDo from "../../CRUD/readToDo";
 
 export default function toDoFormat(parsedData, index){
-    let element = document.querySelector('.content-container');
+    let element = document.querySelector('.task-container');
 
     //building the list of to do's to display to the user
     let itemsArray;
@@ -36,6 +37,14 @@ export default function toDoFormat(parsedData, index){
         deleteButton.setAttribute('projectid', item.projectID);
         deleteButton.setAttribute('todoid', item.id)
         todoBox.appendChild(deleteButton);
+
+        let expandButton = document.createElement('span');
+        expandButton.classList.add('expand-todo')
+        expandButton.classList.add('material-symbols-outlined')
+        expandButton.innerHTML = 'info';
+        expandButton.setAttribute('projectid', item.projectID)
+        expandButton.setAttribute('todoid', item.id)
+        todoBox.appendChild(expandButton);
         
 
         todoBox.classList.add('todo-box');
@@ -44,4 +53,5 @@ export default function toDoFormat(parsedData, index){
     })
 
     deleteToDo();
+    readToDo();
 }
